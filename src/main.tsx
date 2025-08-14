c738ab369a4e6ae11e5db1211b8db23f656e20cb
Ofek Amirav <128730112+ofekamirav@users.noreply.github.com>
2025-08-14 22:16:06 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { CONFIG } from './config';
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider
-      clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID as string}
+     clientId={CONFIG.GOOGLE_CLIENT_ID}
     >
       <App />
     </GoogleOAuthProvider>
